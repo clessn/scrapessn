ef1ae06bf9cf7f28a641648ed9d014fe663bb907
hubcad25 <hubertcadieux@gmail.com>
2024-04-02 15:08:03 -0400

    add try catch in fetch_webpage

--- R/generic.R
+++ R/generic.R
@@ -32,28 +32,54 @@ clean_url_to_id <- function(urls) {
 #' @description Fetches the HTML content of a webpage. It constructs the URL by optionally appending
 #' an item URL identifier to a base URL, and then uses `rvest::read_html()` to download and parse
 #' the HTML content of the page. If the item URL identifier is not provided, it fetches the base URL.
+#' Optionally retries fetching the page if the first attempt fails.
 #'
 #' @param base_url A character string specifying the base URL of the website.
 #' @param item_url_id An optional character string specifying the specific item's URL identifier
 #' to be appended to the base URL for constructing the full URL. Default is NULL, indicating that
 #' only the base URL is used.
+#' @param retry_on_fail A logical value indicating whether to retry fetching the webpage if the
+#' initial attempt fails. Default is FALSE.
+#' @param max_attempts Integer, the maximum number of retry attempts. Default is 10.
+#' @param sleep_seconds Integer, number of seconds to wait between retry attempts. Default is 1.
+#'
 #' @return An HTML document object representing the fetched webpage. This object can be further
 #' processed using various `rvest` functions to extract specific data.
 #' @examples
 #' \dontrun{
 #' base_url <- "https://www.example.com"
 #' item_url_id <- "page.html"
-#' page <- fetch_webpage(base_url, item_url_id)
+#' # Fetching with retry on fail
+#' page <- fetch_webpage(base_url, item_url_id, retry_on_fail = TRUE)
 #' print(page)
 #'
-#' # Fetching content from the base URL only
-#' page_base <- fetch_webpage(base_url)
+#' # Fetching content from the base URL only, without retry
+#' page_base <- fetch_webpage(base_url, retry_on_fail = FALSE)
 #' print(page_base)
 #' }
 #' @export
 #' @importFrom rvest read_html
-fetch_webpage <- function(base_url, item_url_id = NULL) {
+fetch_webpage <- function(base_url, item_url_id = NULL, retry_on_fail = FALSE, max_attempts = 10, sleep_seconds = 1) {
   url <- if (!is.null(item_url_id)) paste0(base_url, item_url_id) else base_url
-  page <- rvest::read_html(url)
-  return(page)
+
+  if (!retry_on_fail) {
+    # Classic approach without retry
+    return(rvest::read_html(url))
+  } else {
+    # Retry mechanism
+    for (attempt in 1:max_attempts) {
+      tryCatch({
+        page <- rvest::read_html(url)
+        return(page)  # If successful, return the page
+      }, error = function(e) {
+        message("Attempt ", attempt, " failed: ", e$message)
+        if (attempt < max_attempts) {
+          message("Retrying in ", sleep_seconds, " seconds...")
+          Sys.sleep(sleep_seconds)
+        } else {
+          stop("Failed to fetch webpage after ", max_attempts, " attempts.")
+        }
+      })
+    }
+  }
 }
